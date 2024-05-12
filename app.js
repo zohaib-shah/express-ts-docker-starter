b84c0222b37a491e6642282455a080e2c4471d7c
zohaib shah <weblearningblog@gmail.com>
2024-05-12 15:45:10 +0500

    fix(starter-app): remove unused app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/',(req, res, next) => {
-    res.send("Root route is working");
-});
-app.listen(3000,() => {
-    console.log("Server started on port 3000");
-});
