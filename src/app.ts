9a4c0d15cc543a72d2dc30338e6f02aada0008ab
zohaib shah <weblearningblog@gmail.com>
2024-05-12 21:09:28 +0500

    feat(starter-app): adding workflow to run formatter

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Application, Request, Response, NextFunction } from "express";
 const app: Application = express();
 
-app.get('/',(req: Request, res: Response, next: NextFunction) => {
-    res.send("Root route is working");
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Root route is working");
 });
 
-app.listen(3000,() => {
-    console.log("Server listening on port 3000");
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
 });
